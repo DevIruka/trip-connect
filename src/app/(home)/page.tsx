5eff6a530a507d75b2d69a4c95ced7e338a09475
cara <cara656513@gmail.com>
2025-01-07 10:57:37 +0900

    🎉 supabase server 파일 생성성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { createClient } from '../../utils/supabase/server';
 
-const Homepage = () => {
-  return <div>home page</div>;
+const Homepage = async () => {
+  const supabase = await createClient();
+  const { data: request_posts, error } = await supabase
+    .from('request_posts')
+    .select('*')
+    .range(0, 9);
+  console.log('request_posts', request_posts);
+  return (
+    <div>
+      <header className="grid">
+        <div className="flex gap-10">
+          <button>홈</button>
+          <button>맛집</button>
+          <button>장소</button>
+          <button>숙소</button>
+          <button>이벤트</button>
+          <button>일정/경비</button>
+        </div>
+      </header>
+      <button>나라 선택하기</button>
+      <button>질문하기</button>
+      <button>랜덤게시물</button>
+    </div>
+  );
 };
 
 export default Homepage;
