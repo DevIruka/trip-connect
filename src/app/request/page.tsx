f5f76c0ebda503177a2bd36bb46dda6b33eea5d0
Raina-Moon <mds6425@gmail.com>
2025-01-07 13:11:14 +0900

    💩 오타 수정함

--- src/app/request/page.tsx
+++ src/app/request/page.tsx
@@ -12,11 +12,11 @@ type FormInputs = {
   deadline: string;
 };
 
-// 도시 더미미 데이터
+// 도시 더미 데이터
 const continents: { name: string; cities: string[] }[] = [
   { name: '아시아', cities: ['서울', '도쿄', '방콕', '싱가포르'] },
   { name: '유럽', cities: ['런던', '파리', '로마', '베를린'] },
-  { name: '북미', cities: ['뉴욕', '로스앤젤레스', '토론토', '시애틀틀'] },
+  { name: '북미', cities: ['뉴욕', '로스앤젤레스', '토론토', '시애틀'] },
   { name: '오세아니아', cities: ['시드니', '멜버른', '오클랜드'] },
   { name: '기타', cities: ['두바이', '이스탄불', '모스크바'] },
 ];
