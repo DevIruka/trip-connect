5a2b8913f3b37af130eafa92537c1a8f110ee80f
DevIruka <qkralswns1998@gmail.com>
2025-01-08 19:30:46 +0900

    🔨 script : 리액트 드롭다운을 설치

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,29 @@
 import Image from 'next/image';
 import { GrSearch } from 'react-icons/gr';
 import { LuBell } from 'react-icons/lu';
-
+import { FiUser } from 'react-icons/fi';
+import Link from 'next/link';
 const logoImage = '/images/logo.png';
 
 const Header = () => {
   return (
     <>
-      <div className="flex flex-row fixed top-0 bg-white z-50 w-[375px] justify-between">
-        <Image
-          src={logoImage}
-          alt="트립 커넥트 로고."
-          width={150}
-          height={50}
-          className="ml-3 p-2"
-        />
-        <div className="flex flex-row items-center justify-center gap-2">
-          <GrSearch size={25} />
+      <div className="flex flex-row fixed top-0 bg-white z-50 w-full justify-between">
+        <Link href="/"
+        className='flex ml-3 mt-2'>
+          <Image
+            src={logoImage}
+            alt="트립 커넥트 로고."
+            width={150}
+            height={50}
+          />
+        </Link>
+        <div className="flex flex-row items-center justify-center gap-4 mr-2">
           <LuBell size={25} />
+          <GrSearch size={25} />
+          <Link href="mypage">
+            <FiUser size={25} />
+          </Link>
         </div>
       </div>
     </>
