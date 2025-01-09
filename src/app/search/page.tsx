a0de56669379cba79214587a4bbbafb0f0ec9049
DevIruka <qkralswns1998@gmail.com>
2025-01-09 14:39:47 +0900

    🩹 fix : PR 템플릿 수정.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { IoIosArrowBack } from 'react-icons/io';
 import { IoMdCloseCircle } from 'react-icons/io';
@@ -13,9 +13,26 @@ const SearchPage = () => {
     formState: { errors },
   } = useForm({ mode: 'onChange' });
 
-  const { isSearching, setIsSearching } = useState<boolean>(true);
+  const query = useRef('');
+  const [recentSearches, setRecentSearches] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return JSON.parse(localStorage.getItem('recentSearches')) || [];
+    }
+    return [];
+  });
+  const handleSearch = () => {
+    const query = queryRef.current.trim(); // ref에서 현재 값 가져오기
+    if (!query) return;
 
-  return !isSearching ? (
+    // 최근 검색어 업데이트
+    const updatedSearches = [
+      query,
+      ...recentSearches.filter((item) => item !== query),
+    ].slice(0, 5);
+    setRecentSearches(updatedSearches);
+    localStorage.setItem('recentSearches', JSON.stringify(updatedSearches));
+  };
+  return (
     <div className="inner">
       <form className="flex flex-row items-center gap-2">
         <Link href="/">
@@ -26,9 +43,9 @@ const SearchPage = () => {
           placeholder="검색어를 입력해주세요"
           className="border border-black rounded-full h-8 w-72 mt-1 mb-1 px-4"
         />
-          <button type="button" className="absolute right-12 top-2">
-            <IoMdCloseCircle size={25}/>
-          </button>
+        <button type="button" className="absolute right-12 top-2">
+          <IoMdCloseCircle size={25} />
+        </button>
         <Link href="/">
           <span className="mr-1">닫기</span>
         </Link>
@@ -40,8 +57,6 @@ const SearchPage = () => {
         </div>
       </div>
     </div>
-  ) : (
-    <></>
   );
 };
 export default SearchPage;
