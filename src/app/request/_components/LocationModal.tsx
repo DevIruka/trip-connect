338be7c8e95b481574d7defc621a572a880f7437
Raina-Moon <mds6425@gmail.com>
2025-01-10 19:09:44 +0900

    ✨ feat : 국가/도시 리스트 공통으로 생성

--- src/app/request/_components/LocationModal.tsx
+++ src/app/request/_components/LocationModal.tsx
@@ -2,20 +2,13 @@
 
 import React from 'react';
 import { Continent } from '../_types/form';
+import { continents } from '@/data/continents';
 
 type Props = {
   isOpen: boolean;
   toggleModal: () => void;
   handleLocationSelect: (location: string) => void;
 };
-
-const continents:Continent[] = [
-  { name: '아시아', cities: ['서울', '도쿄', '방콕', '싱가포르'] },
-  { name: '유럽', cities: ['런던', '파리', '로마', '베를린'] },
-  { name: '북미', cities: ['뉴욕', '로스앤젤레스', '토론토', '시애틀'] },
-  { name: '오세아니아', cities: ['시드니', '멜버른', '오클랜드'] },
-  { name: '기타', cities: ['두바이', '이스탄불', '모스크바'] },
-];
 
 const LocationModal: React.FC<Props> = ({ isOpen, toggleModal, handleLocationSelect }) => {
   if (!isOpen) return null;
