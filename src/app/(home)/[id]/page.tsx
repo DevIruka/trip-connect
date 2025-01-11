d4039b9590aa28f36da360afaa9c6b041144cdc8
cara <cara656513@gmail.com>
2025-01-11 20:24:25 +0900

    💄 style: home page style

--- src/app/(home)/[id]/page.tsx
+++ src/app/(home)/[id]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import Image from 'next/image';
 import Navbar from '../_components/navBar';
 import QnaHeader from '../_components/qnaHeader';
 import { useBookmarkMutations } from '../_hooks/BookmarkHooks';
 import { useBookmarks } from '../_hooks/useBookmark';
 import { usePosts } from '../_hooks/usePosts';
+import bookmarkButton from '../../../../public/images/bookmark.svg';
 
 const CategoryPage = ({ params }: { params: { id: string } }) => {
   const category = params.id;
@@ -20,51 +22,75 @@ const CategoryPage = ({ params }: { params: { id: string } }) => {
       <div className="inner h-[2000px]">
         <QnaHeader />
         <Navbar />
-        {allPosts?.map((post) => {
-          const bookmarked = isPostBookmarked(post.id);
-          return (
-            <div
-              onClick={() => {
-                location.href = `/post/${post.id}`;
-              }}
-              key={post.id}
-              className="border-2 flex cursor-pointer"
-            >
-              <div>
-                <div>{post.title}</div>
-                <div>{post.content}</div>
-                <div>{post.credit}</div>
-                <div>{post.date_end}</div>
-              </div>
-              {bookmarked ? (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    deleteBookmarkMutation.mutate(post.id);
-                  }}
-                >
-                  북마크 해제
-                </button>
-              ) : (
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    addBookmarkMutation.mutate(post.id);
-                  }}
-                >
-                  북마크
-                </button>
-              )}
-            </div>
-          );
-        })}
+        <ul className="">
+          {allPosts?.map((post) => {
+            const bookmarked = isPostBookmarked(post.id);
+            return (
+              <li
+                onClick={() => {
+                  location.href = `/post/${post.id}`;
+                }}
+                key={post.id}
+                className="border-2 rounded-lg p-2 grid cursor-pointer w-full mb-2"
+              >
+                <div>질문글</div>
+                <div className="flex justify-between">
+                  <div className="flex gap-2">
+                    <div>{post.country_city}</div>
+                    <div className="flex gap-2 overflow-hidden">
+                      {post.category
+                        ? post.category.map((category, index) => (
+                            <div key={index}>{category}</div>
+                          ))
+                        : ''}
+                    </div>
+                  </div>
+                  {bookmarked ? (
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deleteBookmarkMutation.mutate(post.id);
+                      }}
+                    >
+                      <Image
+                        width={20}
+                        height={20}
+                        src={bookmarkButton}
+                        alt="bookmark button"
+                        className="brightness-0"
+                      />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        addBookmarkMutation.mutate(post.id);
+                      }}
+                    >
+                      <Image
+                        width={20}
+                        height={20}
+                        src={bookmarkButton}
+                        alt="bookmark button"
+                      />
+                    </button>
+                  )}
+                </div>
+                <div>
+                  <h1 className="text-xl font-bold">{post.title}</h1>
+                  <div>{post.content}</div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
 
         {/* 더보기 버튼 */}
         {hasNextPage && (
           <button
             onClick={() => fetchNextPage()}
             disabled={isFetchingNextPage}
-            style={{ marginTop: '20px', padding: '10px 20px' }}
+            className="border-2 rounded-lg p-2 grid cursor-pointer w-full mb-2"
           >
             {isFetchingNextPage ? '로딩 중...' : '더보기'}
           </button>
