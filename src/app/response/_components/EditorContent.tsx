005d9edfd42042519b18885d79d24915e68fc54b
Raina-Moon <mds6425@gmail.com>
2025-01-13 20:56:24 +0900

    🩹 fix : 답변 게시글 작성시 menubar 무료,유료 공유하도록 수정

--- src/app/response/_components/EditorContent.tsx
+++ src/app/response/_components/EditorContent.tsx
@@ -10,19 +10,11 @@ const EditorContent: React.FC<Props> = ({ editor }) => {
 
   return (
     <div className="border border-gray-300 rounded p-4 min-h-[150px]">
-    {/* 에디터가 비어있을 때 플레이스홀더 표시 */}
-    {editor.isEmpty && (
-      <p className="text-gray-400 select-none">
-        유료 분량의 답변을 작성해 주세요
-      </p>
-    )}
-
-    {/* Tiptap 에디터 콘텐츠 */}
-    <TiptapEditorContent
-      editor={editor}
-      className="w-full h-full outline-none"
-    />
-  </div>
+      <TiptapEditorContent
+        editor={editor}
+        className="w-full h-full outline-none"
+      />
+    </div>
   );
 };
 
