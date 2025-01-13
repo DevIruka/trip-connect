adc90bf4e966255fd918bdf21b979d73eb7ddeba
cara <cara656513@gmail.com>
2025-01-13 16:55:30 +0900

    ♻️ Refactor: post page ssg로 변경, config images 링크에 우리 supabase url 추가가

--- src/utils/api/supabase_api/post/getPostUser.ts
+++ src/utils/api/supabase_api/post/getPostUser.ts
@@ -1,16 +1,15 @@
 import { supabase } from '@/utils/supabase/supabaseClient';
 
-export const getPostUser = async (userId) => {
-  let { data: response_user, error } = await supabase.from('response_posts')
-    .select(`
-    user_id,
-    user (
-      ${userId}
-    )
-  `);
+export const getPostUser = async (userId: string) => {
+  const { data: user, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('id', userId)
+    .single();
+
   if (error) {
     return null;
   }
 
-  return response_user;
+  return user;
 };
