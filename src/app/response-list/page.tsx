992f0f7e28e4c19a84180576b3abd94f42ac5078
cara <cara656513@gmail.com>
2025-01-14 11:01:14 +0900

    ♻️ refactor: params->searchparams로 메인 페이지 리팩터

--- src/app/response-list/page.tsx
+++ src/app/response-list/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import BackButton from '../post/_components/BackBtn';
 
 const responseListPage = () => {
-  return <div>responseListPage</div>;
+  return (
+    <div className="inner text-center">
+      <div className="h-12 flex place-content-between">
+        <BackButton />
+        <div className="grid place-content-center">답변하기</div>
+      </div>
+    </div>
+  );
 };
 
 export default responseListPage;
