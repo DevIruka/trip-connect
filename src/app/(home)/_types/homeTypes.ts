91b8e9097028b9410b1062b94305c0fc4f72fd43
cara <cara656513@gmail.com>
2025-01-17 00:53:28 +0900

    ♻️ refactor: home page 타입 지정

---
+++ src/app/(home)/_types/homeTypes.ts
@@ -0,0 +1,26 @@
+export type nation = {
+  continent: string;
+  country: string;
+  city: string;
+};
+
+// 게시물 데이터 타입 정의
+export type Post = {
+  created_at: string;
+  id: string | number;
+  title: string;
+  user_id: string;
+
+  category?: string[] | null;
+  content?: string;
+  country_city?: string;
+  credit?: number;
+  date_end?: string;
+  img_url?: string | null;
+
+  content_html?: string;
+  free_content?: string;
+  request_id?: string;
+  request_posts?: { category: string[] };
+  verified_country?: string | null;
+};
