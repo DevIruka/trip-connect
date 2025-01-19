a2311ca7a51b60efc2c69f2bc8da8ec2b28e71de
cara <cara656513@gmail.com>
2025-01-19 21:29:14 +0900

    💄 style: seller-auth, country-verification css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,6 +18,7 @@ const Header = () => {
     '/post',
     '/response-list',
     '/request',
+    '/mypage/seller-auth',
   ];
   const shouldHideHeader = excludedPaths.some((path) =>
     pathname.startsWith(path),
