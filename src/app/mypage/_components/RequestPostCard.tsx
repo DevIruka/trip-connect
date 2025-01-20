78b29f0c5f0966056dc609c62c46cef36b23c135
minjung <mj96k09100@gmail.com>
2025-01-20 09:00:53 +0900

    feat:수정삭제기능 추가

--- src/app/mypage/_components/RequestPostCard.tsx
+++ src/app/mypage/_components/RequestPostCard.tsx
@@ -7,12 +7,15 @@ import { RequestPost } from '../_type/type';
 import TimeAgo from './TimeAgo';
 import stripHtmlTags from '../_util/striptHtmlTags';
 import { supabase } from '@/utils/supabase/supabaseClient';
+import { useRouter } from 'next/navigation';
+
 
 const RequestPostCard: React.FC<{ post: RequestPost }> = ({ post }) => {
+    const router = useRouter(); 
   const dDay = calculateDDay(post.date_end || undefined);
   const plainContent = stripHtmlTags(post.content ?? '');
   const [responseCount, setResponseCount] = useState<number>(0);
-
+  const [showActions, setShowActions] = useState<boolean>(false);
   const fetchResponseCount = useCallback(async () => {
     try {
       const { data, error } = await supabase
@@ -45,43 +48,22 @@ const RequestPostCard: React.FC<{ post: RequestPost }> = ({ post }) => {
       }}
     >
       {/* D-Day와 위치 및 카테고리 */}
-      <div className="flex flex-row items-center gap-[4px]">
-        <div
-          className="flex items-center justify-center text-[#FF810B] text-[12px]"
-          style={{
-            height: '22.017px',
-            padding: '0px 6px',
-            transform: 'rotate(-0.024deg)',
-            borderRadius: '4px',
-            background: '#FFECD4',
-          }}
-        >
-          {dDay}
-        </div>
-        <div
-          className="flex items-center text-[#45484D] text-[12px]"
-          style={{
-            height: '22px',
-            padding: '0px 6px 0px 4px',
-            alignItems: 'center',
-            gap: '2px',
-            borderRadius: '4px',
-            background: '#F5F7FA',
-          }}
-        >
-          <Image
-            src="/images/ic-location.svg"
-            alt="Location"
-            width={12}
-            height={12}
-          />
-          <span>
-            {JSON.parse(post.country_city || '{}').country || '위치 없음'}
-          </span>
-        </div>
-        {post.category.slice(0, 2).map((cat, i) => (
+      <div className="flex flex-row items-center justify-between gap-[4px] w-full">
+        {/* 왼쪽 - D-Day, 위치 및 카테고리 */}
+        <div className="flex flex-row items-center gap-[4px]">
           <div
-            key={i}
+            className="flex items-center justify-center text-[#FF810B] text-[12px]"
+            style={{
+              height: '22.017px',
+              padding: '0px 6px',
+              transform: 'rotate(-0.024deg)',
+              borderRadius: '4px',
+              background: '#FFECD4',
+            }}
+          >
+            {dDay}
+          </div>
+          <div
             className="flex items-center text-[#45484D] text-[12px]"
             style={{
               height: '22px',
@@ -92,9 +74,121 @@ const RequestPostCard: React.FC<{ post: RequestPost }> = ({ post }) => {
               background: '#F5F7FA',
             }}
           >
-            {cat}
+            <Image
+              src="/images/ic-location.svg"
+              alt="Location"
+              width={12}
+              height={12}
+            />
+            <span>
+              {JSON.parse(post.country_city || '{}').country || '위치 없음'}
+            </span>
           </div>
-        ))}
+          {post.category.slice(0, 2).map((cat, i) => (
+            <div
+              key={i}
+              className="flex items-center text-[#45484D] text-[12px]"
+              style={{
+                height: '22px',
+                padding: '0px 6px 0px 4px',
+                alignItems: 'center',
+                gap: '2px',
+                borderRadius: '4px',
+                background: '#F5F7FA',
+              }}
+            >
+              {cat}
+            </div>
+          ))}
+        </div>
+
+        {/* 오른쪽 - 수정 및 삭제 버튼 */}
+        <div className="relative">
+          <button
+            onClick={() => setShowActions((prev) => !prev)}
+            className="p-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="14"
+              height="14"
+              fill="none"
+            >
+              <circle cx="12" cy="5" r="2" fill="#797C80" />
+              <circle cx="12" cy="12" r="2" fill="#797C80" />
+              <circle cx="12" cy="19" r="2" fill="#797C80" />
+            </svg>
+          </button>
+          {showActions && (
+            <div
+              className="absolute top-full right-0"
+              style={{
+                display: 'flex',
+                width: '129px',
+                padding: '8px',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                gap: '10px',
+                borderRadius: '8px',
+                border: '1px solid var(--Grayscale-Gray-8-line, #F4F4F4)',
+                background: '#FFF',
+                boxShadow: '0px 4px 12px 0px rgba(0, 0, 0, 0.05)',
+              }}
+            >
+              {/* 수정하기 버튼 */}
+              <button
+                className="w-full text-left"
+                style={{
+                  display: 'flex',
+                  padding: '6px 10px',
+                  alignItems: 'center',
+                  gap: '10px',
+                  alignSelf: 'stretch',
+                  borderRadius: '8px',
+                  color: 'var(--Grayscale-Gray-1, #45484D)',
+                  textAlign: 'center',
+                  fontFamily: 'Pretendard',
+                  fontSize: '14px',
+                  fontStyle: 'normal',
+                  fontWeight: '500',
+                  lineHeight: 'normal',
+                  letterSpacing: '-0.28px',
+                }}
+                onClick={(e) => {
+                  e.stopPropagation();
+
+                  router.push(`/request-edit/${post.id}`);
+                }}
+              >
+                수정하기
+              </button>
+              {/* 삭제하기 버튼 */}
+              <button
+                className="w-full text-left"
+                style={{
+                  display: 'flex',
+                  padding: '6px 10px',
+                  alignItems: 'center',
+                  gap: '10px',
+                  alignSelf: 'stretch',
+                  borderRadius: '8px',
+                  color: 'var(--Grayscale-Gray-1, #45484D)',
+                  textAlign: 'center',
+                  fontFamily: 'Pretendard',
+                  fontSize: '14px',
+                  fontStyle: 'normal',
+                  fontWeight: '500',
+                  lineHeight: 'normal',
+                  letterSpacing: '-0.28px',
+                }}
+                onClick={() => console.log(`Delete post: ${post.id}`)}
+              >
+                삭제하기
+              </button>
+            </div>
+          )}
+        </div>
       </div>
 
       <div className="flex items-start gap-[6px]">
