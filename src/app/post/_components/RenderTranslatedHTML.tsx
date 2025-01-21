0cbfb9c39d7548333f590ab86e5b639d90ed1146
cara <cara656513@gmail.com>
2025-01-22 08:53:20 +0900

    🚑 chore: 변수명 변경

--- src/app/post/_components/RenderTranslatedHTML.tsx
+++ src/app/post/_components/RenderTranslatedHTML.tsx
@@ -7,8 +7,8 @@ const RenderTranslatedHTML = ({
 }: {
   data: { original: string | undefined; translated: string | undefined };
 }) => {
-  const aaa = parseHtmlToArray(data.translated);
-  return <ContentRenderer parsedArray={aaa} />; //1. 리턴 맵데이터 함수 2. 호출하면 맵데이터로 구글 지도 그리는 컴포넌트를 만들어서 (파일 구분) 3.
+  const contentArray = parseHtmlToArray(data.translated);
+  return <ContentRenderer parsedArray={contentArray} />;
 };
 
 export default RenderTranslatedHTML;
