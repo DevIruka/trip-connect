332e8d5bbb43578db26db8406662090d61650c56
cara <cara656513@gmail.com>
2025-01-22 19:33:16 +0900

    🩹 fix: 상세 페이지 디데이

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -10,6 +10,7 @@ import Icon from '@/components/Icons';
 import Imoji from '@/data/images/ic-imoji.svg';
 import share from '@/data/images/ic-share.svg';
 import BackHeader from '../../../components/BackHeader';
+import PostDday from '@/app/home/_components/dDay';
 
 const DetailPage = async ({ params }: { params: { id: string } }) => {
   const postId = params.id; // URL에서 전달된 게시물 ID
@@ -56,14 +57,14 @@ const DetailPage = async ({ params }: { params: { id: string } }) => {
                 <div>답변 기한</div>
                 <div>크레딧</div>
               </div>
-              <div className="text-black text-sm font-medium leading-tight grid gap-3">
+              <div className="text-black text-sm font-medium leading-tight grid gap-2.5">
                 <div>{`${JSON.parse(post.country_city).country} / ${
                   JSON.parse(post.country_city).city
                 }`}</div>
-                <div className="flex gap-1">
+                <div className="flex gap-1 items-center">
                   {post.date_end}
-                  <div className="px-1.5 bg-[#ffecd4] rounded justify-center items-center inline-flex text-center text-[#ff800a] text-xs font-medium">
-                    D-24
+                  <div className="px-1.5 rounded justify-center items-center inline-flex text-center text-xs font-medium">
+                    <PostDday postDateEnd={post.date_end} />
                   </div>
                 </div>
                 <div>{post.credit} C</div>
