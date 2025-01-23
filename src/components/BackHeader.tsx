de92840b32b4669254f5aae0ef790aedf13686cc
cara <cara656513@gmail.com>
2025-01-23 18:00:21 +0900

    🩹 fix: 수정삭제 기능 수정중

--- src/components/BackHeader.tsx
+++ src/components/BackHeader.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { useUserStore } from '@/store/userStore';
 import DeleteConfirmModal from '@/app/post/_components/DeleteConfirmModal';
 import { Tables } from '@/types/supabase';
+import SelectBox from './SelectBox';
 
 const BackHeader = ({
   image,
@@ -45,46 +46,19 @@ const BackHeader = ({
         )}
 
         {isModalOpen && (
-          <div
-            className="w-full h-screen absolute top-0 right-0 cursor-pointer"
-            onClick={() => setIsModalOpen(!isModalOpen)}
-          >
-            <div className="bg-white grid w-[129px] rounded-lg shadow-[0px_4px_12px_0px_rgba(0,0,0,0.05)] text-center p-2.5 justify-start items-center gap-1 absolute top-[45px] right-[23px] text-[#44484c] text-sm font-medium leading-tight">
-              {user ? (
-                <>
-                  <button
-                    onClick={() => {
-                      if (user.id !== post?.user_id) {
-                        alert('작성자가 아닙니다.');
-                      } else location.href = `/request-edit/${post?.id}`;
-                    }}
-                    className="ml-2.5 mx-[4.5px] h-[29px]"
-                  >
-                    수정하기
-                  </button>
-                  <button
-                    onClick={() => setIsDModalOpen(true)}
-                    className="ml-2.5 mx-[4.5px] h-[29px]"
-                  >
-                    삭제하기
-                  </button>
-                </>
-              ) : (
-                <button
-                  onClick={() => alert('신고하기 기능은 아직 준비중입니다.')}
-                  className="ml-2.5 mx-[4.5px] h-[29px]"
-                >
-                  신고하기
-                </button>
-              )}
-            </div>
-          </div>
+          <SelectBox
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+            user={user!}
+            post={post!}
+            setIsDModalOpen={setIsDModalOpen}
+          />
         )}
       </div>
       <DeleteConfirmModal
         isOpen={isDModalOpen}
         onClose={() => setIsDModalOpen(false)}
-        post={post!}
+        requestpost={post!}
       />
     </>
   );
--- src/utils/api/supabase_api/home/fetchPostDelete.ts
+++ src/utils/api/supabase_api/home/fetchPostDelete.ts
@@ -16,6 +16,7 @@ export const fetchPostDelete = async (
       if (error) throw error;
 
       alert('삭제되었습니다.');
+      location.href = '/'; // 삭제 후 홈으로 리다이렉트
     } catch (error) {
       console.error('Error:', error);
       alert('답변글이 있거나, 삭제 중 문제가 발생했습니다.');
