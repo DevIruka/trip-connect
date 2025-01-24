e2d474690e38f56f31a3db2b84696fa353aa2de5
DevIruka <qkralswns1998@gmail.com>
2025-01-24 17:34:41 +0900

    🚑 fix : route.ts 원복.

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -17,44 +17,23 @@ export async function GET(request: Request) {
       const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'production';
 
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
+      // const {
+      //   data: { user },
+      // } = await supabase.auth.getUser();
 
-      const { data: profile } = await supabase
-            .from('users') // 유저 정보를 저장하는 테이블 (예: profiles)
-            .select('nickname')
-            .eq('id', user?.id) // user.id를 기준으로 검색
-            .single();
+      // const { data: profile } = await supabase
+      //   .from('users') // 유저 정보를 저장하는 테이블 (예: profiles)
+      //   .select('nickname')
+      //   .eq('id', user?.id) // user.id를 기준으로 검색
+      //   .single();
 
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        if (profile) {
-
-          if (profile?.nickname === 'guest') {
-            // 닉네임이 없으면 설정 페이지로 이동
-            return NextResponse.redirect(`${origin}/auth`);
-          }
-          return NextResponse.redirect(`${origin}${next}`);
-        }
+        return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        if (profile) {
-
-          if (profile?.nickname === 'guest') {
-            // 닉네임이 없으면 설정 페이지로 이동
-            return NextResponse.redirect(`https://${forwardedHost}${next}/auth`);
-          }
-          return NextResponse.redirect(`https://${forwardedHost}${next}/auth`);
-        }
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
-        if (profile) {
-
-          if (profile?.nickname === 'guest') {
-            // 닉네임이 없으면 설정 페이지로 이동
-            return NextResponse.redirect(`https://${origin}${next}/auth`);
-          }
-          return NextResponse.redirect(`https://${origin}${next}/auth`);
-        }
+        return NextResponse.redirect(`https://${origin}${next}`);
       }
     }
   }
