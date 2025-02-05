3d904be1cbe1b527d342c13aa17a4df5366402b5
cara <cara656513@gmail.com>
2025-02-05 15:59:33 +0900

    🩹 fix: 게시물 한/영 설정에 따라 원문/번역본 나오게끔

--- src/app/post/_hooks/getGPTTranslator.tsx
+++ src/app/post/_hooks/getGPTTranslator.tsx
@@ -16,7 +16,7 @@ export const getGPTTranslator = async (text: string, nation?: string) => {
             type: 'text',
             text: `언어를 감지해서 사용자의 언어가 아니면, 사용자의 언어로 번역해주세요. 사용자 언어: ${
               nation ? nation : '한국'
-            }어\nhtml 문법상 텍스트만 번역해주세요. 나머지는 그대로 되돌려주세요. 만일 html 태그가 없다면, <p></p>태그로 감싸서 되돌려주세요. \n아래의 JSON 형식으로\n---\n{\n  "original": <p>hi</p>,\n "translated": <p>안녕</p>\n}`,
+            }어(두바이, 싱가포르는 영어)\nhtml 문법상 텍스트만 번역해주세요. 나머지는 그대로 되돌려주세요. 만일 html 태그가 없다면, <p></p>태그로 감싸서 되돌려주세요. \n아래의 JSON 형식으로\n---\n{\n  "original": <p>hi</p>,\n "translated": <p>안녕</p>\n}`,
           },
         ],
       },
